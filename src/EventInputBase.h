e2ac1bb5cccc8cde275fc1ad909026ddf04cf00a
Philip Fletcher <philip.fletcher@stutchbury.com>
2025-02-03 15:57:12 +0000

    Move InputEventType definition to InputEvents.h

--- src/EventInputBase.h
+++ src/EventInputBase.h
@@ -17,45 +17,6 @@
     #include <functional>
 #endif
 
-// Some platforms stupidly use #define for DISABLED (ahem, espressif), so replace it with a constexpr variable
-// Many thanks to @kfoltman https://github.com/kfoltman for the workaround.
-#ifdef DISABLED
-constexpr auto ACTUAL_DISABLED_VALUE=DISABLED;
-#undef DISABLED
-constexpr auto DISABLED=ACTUAL_DISABLED_VALUE;
-#endif
-
-constexpr size_t NUM_EVENT_TYPE_ENUMS = 19; 
-enum class InputEventType : uint8_t {
-    //Common
-    ENABLED,
-    DISABLED,
-    IDLE,
-    //Button, Encoder Button
-    PRESSED,
-    RELEASED,
-    CLICKED,
-    DOUBLE_CLICKED,
-    MULTI_CLICKED,
-    LONG_CLICKED,
-    LONG_PRESS,
-    //Encoder, Analog
-    CHANGED,
-    //Encoder Button (maybe joystick button?)
-    CHANGED_PRESSED,
-    CHANGED_RELEASED,
-    //Joystick
-    CHANGED_X,
-    CHANGED_Y,
-    //Switch
-    ON,
-    OFF,
-    //TouchScreen
-    DRAG,
-    DRAGGED
-};
-
-
 
 
 class EventInputBase {
