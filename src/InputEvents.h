6af51448557a70acfcf436d386333eae87036c7e
Philip Fletcher <philip.fletcher@stutchbury.com>
2025-02-11 13:18:45 +0000

    Reinstate #define  (commented out for testing...)

--- src/InputEvents.h
+++ src/InputEvents.h
@@ -21,13 +21,13 @@
     #endif
 #endif
 
-// #if defined(__has_include) // Check if __has_include is supported
-//     #if !__has_include(<EncoderAdapter.h>)
-//         #ifndef EXCLUDE_EVENT_ENCODER //might be defined by build_flags
-//             #define EXCLUDE_EVENT_ENCODER
-//         #endif
-//     #endif
-// #endif
+#if defined(__has_include) // Check if __has_include is supported
+    #if !__has_include(<EncoderAdapter.h>)
+        #ifndef EXCLUDE_EVENT_ENCODER //might be defined by build_flags
+            #define EXCLUDE_EVENT_ENCODER
+        #endif
+    #endif
+#endif
 
 
 /// \cond DO_NOT_DOCUMENT
