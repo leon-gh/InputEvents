e59feafa10305ca82af7fc3dc13e465c18433eae
Philip Fletcher <philip.fletcher@stutchbury.com>
2025-06-25 15:26:44 +0100

    Add 'NONE' as an InputEventType

--- src/InputEvents.h
+++ src/InputEvents.h
@@ -53,25 +53,26 @@ constexpr size_t NUM_EVENT_TYPE_ENUMS = 19;
  * @brief A list of all events that can be fired by InputEvents classes.
  */
 enum class InputEventType : uint8_t {
-    ENABLED,            ///< Fired by all imputs
-    DISABLED,           ///< Fired by all imputs
-    IDLE,               ///< Fired by all imputs
-    PRESSED,            ///< Fired by EventButton, EventEncoderButton and EventTouchScreen
-    RELEASED,           ///< Fired by EventButton, EventEncoderButton and EventTouchScreen
-    CLICKED,            ///< Fired by EventButton, EventEncoderButton and EventTouchScreen
-    DOUBLE_CLICKED,     ///< Fired by EventButton, EventEncoderButton and EventTouchScreen
-    MULTI_CLICKED,      ///< Fired by EventButton, EventEncoderButton and EventTouchScreen
-    LONG_CLICKED,       ///< Fired by EventButton, EventEncoderButton and EventTouchScreen
-    LONG_PRESS,         ///< Fired by EventButton, EventEncoderButton and EventTouchScreen
-    CHANGED,            ///< Fired by EventEncoder and EventAnalog
-    CHANGED_PRESSED,    ///< Fired by EventEncoderButton
-    CHANGED_RELEASED,   ///< Fired by EventEncoderButton
-    CHANGED_X,          ///< Fired Bby EventJoystick
-    CHANGED_Y,          ///< Fired Bby EventJoystick
-    ON,                 ///< Fired by EventSwitch
-    OFF,                ///< Fired by EventSwitch
-    DRAGGED,            ///< Fired by [EventTouchScreen]() (experimental)
-    DRAGGED_RELEASED    ///< Fired by EventTouchScreen (experimental)
+    NONE,               ///<  0 Used for initialising/testing
+    ENABLED,            ///<  1 Fired by all imputs
+    DISABLED,           ///<  2 Fired by all imputs
+    IDLE,               ///<  3 Fired by all imputs
+    PRESSED,            ///<  4 Fired by EventButton, EventEncoderButton and EventTouchScreen
+    RELEASED,           ///<  5 Fired by EventButton, EventEncoderButton and EventTouchScreen
+    CLICKED,            ///<  6 Fired by EventButton, EventEncoderButton and EventTouchScreen
+    DOUBLE_CLICKED,     ///<  7 Fired by EventButton, EventEncoderButton and EventTouchScreen
+    MULTI_CLICKED,      ///<  8 Fired by EventButton, EventEncoderButton and EventTouchScreen
+    LONG_CLICKED,       ///<  9 Fired by EventButton, EventEncoderButton and EventTouchScreen
+    LONG_PRESS,         ///< 10 Fired by EventButton, EventEncoderButton and EventTouchScreen
+    CHANGED,            ///< 11 Fired by EventEncoder and EventAnalog
+    CHANGED_PRESSED,    ///< 12 Fired by EventEncoderButton
+    CHANGED_RELEASED,   ///< 13 Fired by EventEncoderButton
+    CHANGED_X,          ///< 14 Fired Bby EventJoystick
+    CHANGED_Y,          ///< 15 Fired Bby EventJoystick
+    ON,                 ///< 16 Fired by EventSwitch
+    OFF,                ///< 17 Fired by EventSwitch
+    DRAGGED,            ///< 18 Fired by [EventTouchScreen]() (experimental)
+    DRAGGED_RELEASED    ///< 19 Fired by EventTouchScreen (experimental)
 };
 
 #endif
